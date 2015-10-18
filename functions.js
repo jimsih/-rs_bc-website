eb66409372722fd038d397ff4bf5ec92ceba03ec
Robert <robert.grundstrom@gmail.com>
2015-10-18 15:47:49 +0200

    Äntligen lägger jag upp den

---
+++ functions.js
@@ -0,0 +1,56 @@
+function loadXMLDoc(filename)
+{
+if (window.ActiveXObject)
+  {
+  xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  }
+else
+  {
+  xhttp = new XMLHttpRequest();
+  }
+xhttp.open("GET", filename, false);
+try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
+xhttp.send("");
+return xhttp.responseXML;
+}
+
+function displayResult(file,style,target)
+{
+xml = loadXMLDoc(file+".xml");
+xsl = loadXMLDoc(style+".xsl");
+// code for IE
+if (window.ActiveXObject || xhttp.responseType == "msxml-document")
+  {
+  ex = xml.transformNode(xsl);
+  document.getElementById(target).innerHTML = ex;
+  }
+// code for Chrome, Firefox, Opera, etc.
+else if (document.implementation && document.implementation.createDocument)
+  {
+  xsltProcessor = new XSLTProcessor();
+  xsltProcessor.importStylesheet(xsl);
+  resultDocument = xsltProcessor.transformToFragment(xml, document);
+  document.getElementById(target).appendChild(resultDocument);
+  }
+}
+function replaceResult(file,style,target)
+{
+document.getElementById(target).removeChild(document.getElementById(target).firstChild);
+displayResult(file,style,target);
+}
+function replaceTwoResult(file1,style1,target1,file2,style2,target2)
+{
+replaceResult(file1,style1,target1);
+replaceResult(file2,style2,target2);
+}
+function setUpDouble(file1,style1,target1,file2,style2,target2,file3,style3,target3)
+{
+displayResult(file1,style1,target1);
+displayResult(file2,style2,target2);
+displayResult(file3,style3,target3);
+}
+function setUp(file1,style1,target1,file2,style2,target2)
+{
+displayResult(file1,style1,target1);
+displayResult(file2,style2,target2);
+}
---
+++ sv/association/side1.xsl
@@ -0,0 +1,31 @@
+<xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:variable name="lang">sv</xsl:variable>
+<xsl:template match="/">
+  <html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/stylesheet.css"/>
+    <script type="text/javascript" src="functions.js"></script>
+    </head>
+    <body>
+    <h2><xsl:value-of select="root/head/members[@lang=$lang]"/></h2>
+    <table>
+
+      <tr>
+        <th><xsl:value-of select="root/head/member[@lang=$lang]"/></th>
+        <th><xsl:value-of select="root/head/office[@lang=$lang]"/></th>
+      </tr>
+      <xsl:for-each select="root/member">
+      <tr>
+        <td><xsl:value-of select="name[@lang=$lang]"/></td>
+        <td><xsl:value-of select="office[@lang=$lang]"/></td>
+      </tr>
+      </xsl:for-each>
+      <tr>
+       <td><button onclick="replaceResult('members','members','content')">SWITCH</button></td>
+      </tr>
+    </table>
+    </body>
+  </html>
+</xsl:template>
+</xsl:stylesheet>
