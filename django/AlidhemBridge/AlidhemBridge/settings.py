dd85e0744f6fffe5f8922981664e02f18b5ab624
Jimmy Sihlberg <jimmy.sihlberg@gmail.com>
2017-09-03 14:05:36 +0200

    Added STATIC_ROOT in settings

--- django/AlidhemBridge/AlidhemBridge/settings.py
+++ django/AlidhemBridge/AlidhemBridge/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-print(MEDIA_ROOT)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
